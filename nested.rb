2a2b77c9e65d755d44c4df59f24d4fb7b6ac0056
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-20 18:14:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,5 @@ def adding_to_dennis
         }
      }
 		 programmer_hash[:dennis_ritchie][:languages] = "Assembly"
-		 programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages][0]
 end
